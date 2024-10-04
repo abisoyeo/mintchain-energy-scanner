2053010fc96e1833ab14fef4894b6b2d38a3e906
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2024-10-04 13:30:26 +0100

    Add project files.

---
+++ MintChainDropsApi/Controllers/Drops.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MintChainDropsApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MintChainDropsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Drops : ControllerBase
+    {
+        // GET: api/<Drops>
+        [HttpGet]
+        public IEnumerable<string> Get(DropsModel dropsModel)
+        {
+            /*
+             * Hitting this endpoint, pass the auth, and other values needed to call mint api
+             * 
+             * assign those values to the class here
+             * create a function to now query mint api passing in the class
+             * in the function you use a loop to 
+             */
+
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
---
+++ MintChainDropsApi/Models/DropsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MintChainDropsApi.Models
+{
+    public class DropsModel
+    {
+        public string  AuthKey { get; set; }
+        public int StartRange { get; set; }
+        public int EndRange { get; set; }
+        public int MinDrop { get; set; }
+
+    }
+}
